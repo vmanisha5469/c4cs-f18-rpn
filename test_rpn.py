ed3f1a44523297075db2c3c0e905222a2ec896b2
Manisha Vupputuri <vmanisha@umich.edu>
2018-10-26 15:16:15 -0400

    added value error test

--- test_rpn.py
+++ test_rpn.py
@@ -8,3 +8,6 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate("4 3 -")
         self.assertEqual(1, result)
+    def test_toomany(self):
+        with self.asserRaises(ValueError):
+            result = rpn.calculate("1 2 3 +")
